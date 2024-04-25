e93c3b31bccde71c5bb9c1340126586adfdd604a
Shin <emily90418@gmail.com>
2024-04-25 17:04:03 +0800

    修改comment

--- Urban-life-next-bs5/components/article/comment.js
+++ Urban-life-next-bs5/components/article/comment.js
@@ -5,7 +5,8 @@ export default function Comment() {
     <>
       
       <div className="d-flex  mx-5 ">
-        <div className=" d-flex m-3">
+        {/* userimg  */}
+        <div className="col-lg-1 col-md-1 userimg-col-12 ">
           <div className="img-container ">
             <img
               className="w-100  img-thumbnail"
@@ -13,15 +14,22 @@ export default function Comment() {
             />
           </div>
 
-          <p className="mx-3">王小明</p>
+          <span className=" ">王小明</span>
         </div>
         {/* usercontent  */}
-        <div className="ms-3 w-100">
+        <div className="col-10 ms-3 d-none d-sm-none d-lg-block">
           <h5 className="div-7">2024/04/24 11:03</h5>
           <p className="">
             COMMENT美貓校學元肉荷怎天兌裝主化麼杯三有，士雪着原公具。娘國鴨讀里美細蛋乙送爬用占，亮讀拉來方蛋抓和固，以枝停候穴以古馬鴨安是明歌起錯隻扒石止珠，子道呢。新冰第躲話。寺荷麻巾員告要氣又教不枝童，室休頁話早汗跑斥買禾昌事英欠鳥珠！歌路別想？動雨助土尼枝才屋村？紅光羽。交已朵相花位常那個乍澡山肉畫，安乞背乞三汁，巾找中做雪孝已忍在造喜吹六娘習昔要。現細個書采王音。右頭事員爬歌。
           </p>
         </div>
+        <div className="col-lg-11 col-md-11 ms-3  d-lg-none d-sm-block">
+          <h5 className="div-7">2024/04/24 11:03</h5>
+          <p className="">
+            COMMENT美貓校學元肉荷怎天兌裝主化麼杯三有，士雪着原公具。娘國鴨讀里美細蛋乙送爬用占，亮讀拉來方蛋抓和固，以枝停候穴以古馬鴨安是明歌起錯隻扒石止珠，子道呢。
+          </p>
+        </div>
+
       </div>
 
       <style jsx>{`
@@ -48,6 +56,18 @@ export default function Comment() {
           object-fit: cover; /* 保證圖像覆蓋整個容器，但不變形 */
           border-radius: 50%; /* 圓角 */
           
+        }
+        @media (max-width: 500px) {
+          .col-12{
+            width: 100%;
+          }
+          
+          .usercontent{
+            font-size: 14px;
+          }
+          .fs-15 {
+            font-size: 14px;
+          }
         }
       `}</style>
     </>
