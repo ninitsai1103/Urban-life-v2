7a651a3be4883a7ed9bb72b2f1dafcc467f55ab8
Ni <daisytsai11336@gmail.com>
2024-04-28 19:33:16 +0800

    購物車優惠券條件渲染，可撈出未使用且未過期的優惠券，UI待修

--- Urban-life-next-bs5/components/cart/checkout-coupons.js
+++ Urban-life-next-bs5/components/cart/checkout-coupons.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import styles from './cart-checkout.module.css'
 import { FaCircleQuestion } from 'react-icons/fa6'
+import coupons from '@/data/coupon.json'
+import { useCheckout } from '@/hooks/use-checkout'
+import category from '@/data/products-lectures-test/product-category.json'
 
 export default function CheckoutCouponsSelect() {
+  const { items, totalPrice } = useCheckout()
+
+  const couponNotExpired = coupons.filter(
+    (v) => new Date(v.deadline) > new Date() && v.status === '可使用'
+  )
+
+  // const [couponCanUse, setCouponCanUse] = useState(couponNotExpired)
+
+  // useEffect(() => {
+  //   const nextCouponCanUse = couponCanUse.map((v) => {
+  //     if (v.category === category.id &&
+  //         v.scope === category.primary_name &&
+  //         totalPrice >= v.min_price)
+  //           return { ...v, disabled: false }
+  //   })
+  //   console.log(nextCouponCanUse)
+  //   setCouponCanUse(nextCouponCanUse)
+  // }, [totalPrice])
+
+  // const handleCouponCanUse = () => {
+  //   const nextCouponCanUse = items.map((v) => {
+  //     if(v.category === category.id && coupons.scope === category.primary_name && totalPrice >= coupons.min_price){
+  //       return { ...v, disabled: false }
+  //     }else{
+  //       return { ...v, disabled: true }
+  //     }
+  //   })
+  //   setCouponCanUse(nextCouponCanUse)
+  // }
+
   return (
     <>
       {/* <div className="input-group mb-2">
@@ -20,35 +53,50 @@ export default function CheckoutCouponsSelect() {
         <div className="d-none">
           <p>沒有優惠卷</p>
         </div>
-        <div className="d-flex justify-content-between py-2">
-          <div
-            className="form-check-label d-flex align-items-center"
-            for="exampleRadios1"
-          >
-            <input
-              className="form-check-input"
-              type="checkbox"
-              name="exampleRadios"
-              id="exampleRadios1"
-              value="option1"
-            />
-            <div className={styles.coupon_name}>優惠券名稱</div>
-          </div>
-          <div className={styles.coupon_detail} for="exampleRadios1">
-            低銷金額
-          </div>
-          <div className={styles.coupon_detail} for="exampleRadios1">
-            有效期限
-          </div>
-          <div className={styles.coupon_question} for="exampleRadios1">
-            <button className="btn btn-detail d-flex">
-              <div>
-                <FaCircleQuestion />
+        {/* 只顯示未過期且還沒使用的優惠卷 */}
+        {couponNotExpired.map((v, i) => {
+          return (
+            <div className="row  py-2" key={i}>
+              <div
+                className="form-check-label d-flex align-items-center col-3"
+                htmlFor="exampleRadios1"
+              >
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="exampleRadios"
+                  id="exampleRadios1"
+                  value="option1"
+                  disabled={v.disabled}
+                />
+                <div className={styles.coupon_name}>{v.name}</div>
               </div>
-              <div className={styles.coupon_limit}>使用限制</div>
-            </button>
-          </div>
-        </div>
+              <div
+                className={`col-3 ${styles.coupon_detail}`}
+                htmlFor="exampleRadios1"
+              >
+                低消：{v.min_price}元
+              </div>
+              <div
+                className={`col-3 ${styles.coupon_detail}`}
+                htmlFor="exampleRadios1"
+              >
+                有效期限：{v.deadline}
+              </div>
+              <div
+                className={`col-3 ${styles.coupon_question}`}
+                htmlFor="exampleRadios1"
+              >
+                <button className="btn btn-detail d-flex">
+                  <div>
+                    <FaCircleQuestion />
+                  </div>
+                  <div className={styles.coupon_limit}>使用限制</div>
+                </button>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </>
   )
