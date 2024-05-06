6d4e01d8cf255e968cd48d492179a47abdb01278
Ni <daisytsai11336@gmail.com>
2024-05-06 14:02:47 +0800

    購物車中，點選同訂購人radio自動帶入會員資料，並disabled。修正use-member-info勾子，導出並提供他，謝謝舒晴><

--- Urban-life-next-bs5/components/cart/info-pay-info.js
+++ Urban-life-next-bs5/components/cart/info-pay-info.js
@@ -1,19 +1,26 @@
 import { useState } from 'react'
 import styles from './info-pay.module.css'
+import { useMemberInfo } from '@/hooks/use-member-info'
 
 export default function InfoPayPage() {
-  // 切換同訂購人和修正收件人 開始
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [address, setAddress] = useState('')
-  const [email, setEmail] = useState('')
-  // 切換同訂購人和修正收件人 結束
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    phone: '',
+    address: '',
+    email: '',
+  })
+
+  const handleFieldChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value })
+  }
   //訂購人與收件人radio與input狀態變化
   const [same, setSame] = useState(false)
   //抓取localStorage的會員資訊
   // const memberInfo = JSON.parse(window.localStorage.getItem('member-info'))
   //如果有取得localStorage的會員資訊中的id，就從後端取得會員的名字、電話、地址、電子信箱
   // let url = `http://localhost:3000/api/member/${memberInfo.id}`
+  const {user} = useMemberInfo()
+
   return (
     <>
       <h4 className="text-light bg-primary4 p-2 mt-2">訂購人資訊</h4>
@@ -22,19 +29,19 @@ export default function InfoPayPage() {
         <tbody>
           <tr>
             <th className="text-end">姓名：</th>
-            <td className={styles.info_pay_td}>蔡妮妮</td>
+            <td className={styles.info_pay_td}>{user?.name}</td>
           </tr>
           <tr>
             <th className="text-end">電話：</th>
-            <td className={styles.info_pay_td}>0970888777</td>
+            <td className={styles.info_pay_td}>{user?.phone}</td>
           </tr>
           <tr>
             <th className="text-end">地址：</th>
-            <td className={styles.info_pay_td}>桃園市中壢區新生路二段421號</td>
+            <td className={styles.info_pay_td}>{user?.address}</td>
           </tr>
           <tr>
             <th className="text-end">電子信箱：</th>
-            <td className={styles.info_pay_td}>Ninitsai1103@test.com</td>
+            <td className={styles.info_pay_td}>{user?.email}</td>
           </tr>
         </tbody>
       </table>
@@ -47,7 +54,15 @@ export default function InfoPayPage() {
           id="same"
           value="option1"
           checked={same}
-          onChange={() => setSame(true)}
+          onChange={() => {
+            setSame(true)
+            setUserInfo({
+              name: user?.name,
+              phone: user?.phone,
+              address: user?.address,
+              email: user?.email,
+            })
+            }}
         />
         <label className="form-check-label" htmlFor="same">
           同訂購人
@@ -77,9 +92,11 @@ export default function InfoPayPage() {
                 type="text"
                 name="name"
                 id="name"
+                value={same ? user?.name : ''}
                 placeholder="請輸入收件人姓名"
                 autoFocus
                 disabled={same}
+                onChange={handleFieldChange}
               />
             </td>
           </tr>
@@ -91,8 +108,10 @@ export default function InfoPayPage() {
                 type="text"
                 name="phone"
                 id="phone"
+                value={same ? user?.phone : ''}
                 placeholder="請輸入收件人電話"
                 disabled={same}
+                onChange={handleFieldChange}
               />
             </td>
           </tr>
@@ -104,8 +123,10 @@ export default function InfoPayPage() {
                 type="text"
                 name="address"
                 id="address"
+                value={same ? user?.address : ''}
                 placeholder="請輸入收件人地址"
                 disabled={same}
+                onChange={handleFieldChange}
               />
             </td>
           </tr>
@@ -117,8 +138,10 @@ export default function InfoPayPage() {
                 type="email"
                 name="email"
                 id="email"
+                value={same ? user?.email : ''}
                 placeholder="請輸入收件人電子信箱"
                 disabled={same}
+                onChange={handleFieldChange}
               />
             </td>
           </tr>
