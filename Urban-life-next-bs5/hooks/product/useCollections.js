55798e0e9b1647fbd6ca84405692290e5ff88b52
Shin <emily90418@gmail.com>
2024-05-14 15:01:16 +0800

    CRD文章已完成

--- Urban-life-next-bs5/hooks/product/useCollections.js
+++ Urban-life-next-bs5/hooks/product/useCollections.js
@@ -82,8 +82,55 @@ export default function useColloections() {
   }
 
 
+    // 新增文章收藏
+    const addArticleCollection = (articleId) => {
+      api
+        .get(`/collection/add/article/${articleId}`)
+        .then((response) => {
+          console.log(response)
+          const updatedArticle = {
+            ...response.data.data,
+            valid:Number(response.data.data.valid) 
+          }
+          return updatedArticle
+          // 在原有收藏資料的基礎上添加新的文章ID
+        })
+        .catch((error) => {
+          console.log('Error adding article collection:', error)
+          if (error.response) {
+            if (error.response.status === 409) {
+              throw new Error('文章已在收藏中。');
+            } else if (error.response.status === 401) {
+              throw new Error('尚未登入');
+            } else {
+              throw new Error('操作失敗，請稍後再試。');
+            }
+          } else {
+            throw error; // 將非 HTTP 響應錯誤重新拋出
+          }
+        })
+    }
+  
+    // 取消文章收藏
+    const removeArticleCollection = (articleId) => {
+      api.get(`/collection/remove/article/${articleId}`).then((response) => {
+        console.log(response)
+        // 從收藏資料中移除特定文章ID
+        setCollections((prev) => prev.filter((id) => id !== Number(articleId)))
+      })
+    }
+  
+    return {
+      collections,
+      addCollection,
+      removeCollection,
+      addArticleCollection,
+      removeArticleCollection,
+    }
+  }
+  
 
-  return { collections, addCollection, removeCollection}
-}
+  
+
 
 
